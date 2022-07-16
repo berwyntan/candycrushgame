6d678d3acedcf828e3268149f59a2a17e4d74942
Berwyn Tan <berwyntan@gmail.com>
2022-07-16 23:12:47 +0800

    create a random generated board of 4 types of candies

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
+const BOARDWIDTH = 5;
+const BOARDHEIGHT = 5;
+const $container = $(".container");
+let itemMap = [];
+
 const main = () => {
-    console.log($);
+    
+   for (let j=0; j<BOARDHEIGHT; j++) {
+        const $row = $("<div>").addClass(`row${j}`);
+        $row.addClass("row");
+        let itemRow = [];
+        for (let i=0; i<BOARDWIDTH; i++) {
+            // if i>=2, while assigned item ===
+            // itemRow[i-1] && itemRow[i-2]==itemRow[i-1]
+            // recalculate assigned item
+         
+            const randInt = Math.ceil(Math.random()*4);
+            const $item = $("<div>").addClass(`item${randInt}`);
+            $item.addClass(`col${i}`);
+            $row.append($item);
+            itemRow.push(randInt);
+        }
+        $container.append($row);
+        itemMap.push(itemRow);
+    }
+
+    console.log(itemMap);
+    // create a score checker to remove scoring items
+
+    
+    
+
 };
 
 $(main);
