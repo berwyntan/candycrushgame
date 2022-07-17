d1c40f737e145b7b097b84ac987da931df5ba975
Berwyn Tan <berwyntan@gmail.com>
2022-07-17 14:32:00 +0800

    rendered game board on webpage

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const createEmptyArray = () => {
     return itemMap;        
 }
 
-const fillRowsTopTwo = (array) => {
+const fillRowsTopTwo = array => {
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < BOARDWIDTH; i++) {
             const candy = CANDIES[Math.floor(Math.random() * CANDIES.length)];
@@ -26,7 +26,7 @@ const fillRowsTopTwo = (array) => {
     return array;     
 }
 
-const fillRowsThreeAndBelow = (array) => {
+const fillRowsThreeAndBelow = array => {
     for (let j = 2; j < BOARDHEIGHT; j++) {
         for (let i = 0; i < BOARDWIDTH; i++) {
             let candyAvailable = CANDIES.map((candy) => candy);
@@ -42,60 +42,32 @@ const fillRowsThreeAndBelow = (array) => {
             }
             const candy = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
             array[j][i] = candy;
-            // array[j][i] = candy;
         }
     }        
 }
 
-const main = () => {
-    
+const render = array => {
+    const $gameBoard = $("<div>").addClass("game-board");
+    for (let j = 0; j < array.length; j++) {
+        const $row = $("<div>").addClass("row");
+        for (let i=0; i < array[j].length; i++) {
+            const $candyImg = $("<div>").addClass(`${array[j][i]}`);
+            $candyImg.addClass(`row${j}`);
+            $candyImg.addClass(`col${i}`);
+            $row.append($candyImg);
+        }
+        $gameBoard.append($row);
+    }
+    return $gameBoard;
+}
 
-    // create a row wo 3 in a row
-    // create another row wo 3 in a row
-    // create another row wo 3 in a row & wo 3 in a col
-    // repeat
-    // NOTE create in array first before using jquery and html
-
-    // create randomized board
-//    for (let j=0; j<BOARDHEIGHT; j++) {
-//         const $row = $("<div>").addClass(`row${j}`);
-//         $row.addClass("row");
-//         let itemRow = [];
-//         for (let i=0; i<BOARDWIDTH; i++) {
-            
-//             // prevent 3 in a row
-//             let randInt = Math.ceil(Math.random()*CANDIES);    
-//             if (i>=2) {
-//                 while (randInt===itemRow[i-1] && itemRow[i-2]===itemRow[i-1]) {                
-//                     randInt = Math.ceil(Math.random()*CANDIES);   
-//                     }
-//                 }
-            
-        
-//             const $item = $("<div>").addClass(`item${randInt}`);
-//             $item.addClass(`col${i}`);
-//             $row.append($item);
-//             itemRow.push(randInt);
-            
-//         }
-//         $container.append($row);
-//         itemMap.push(itemRow);
-//     }
-//     // check 3 in a column
-//     for (let j=2; j<BOARDHEIGHT; j++) {
-//         for (let i=0; i<BOARDWIDTH; i++) {
-//             if (itemMap[j][i]===itemMap[j-1][i] && itemMap[j-2][i]===itemMap[j-1][i]) {
-//                 console.log("3 in a col")
-                
-//             }
-//         }
-        
-//     }
+const main = () => {    
 
     let newGameArray = createEmptyArray();
     fillRowsTopTwo(newGameArray);
     fillRowsThreeAndBelow(newGameArray);
     console.log(newGameArray);
+    $container.append(render(newGameArray));
     // create a score checker to remove scoring items
 
     
