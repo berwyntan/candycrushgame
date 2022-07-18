f2214778dfab51c1cfd1d16bd4c9392650e66cb0
Berwyn Tan <berwyntan@gmail.com>
2022-07-18 16:03:27 +0800

    saved

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const $container = $(".container");
 // run thru the 12 patterns to check available moves
 // try out the draggable and droppable jquery
 class Candy {
-    constructor(name, row, col, target=false, clickable=false) {
+    constructor(name, row, col, target=false, clickable=false, remove=false) {
         this.name = name;
         this.row = `row${row}`;
         this.col = `col${col}`;
         this.target = target;
         this.clickable = clickable;
+        this.remove = remove;
     }
 }
 // make a non empty totally random array
@@ -26,60 +27,100 @@ const createRandomArray = () => {
             // itemRow.push("empty");
             const candyRandom = CANDIES[Math.floor(Math.random() * CANDIES.length)];
             itemRow[i] = new Candy(candyRandom, j, i);
+            
         }
         itemMap.push(itemRow);
     }
     return itemMap;        
 }
-// filter the candies
+// filter the candies -- not working----------------
 const filterRows = array => {
     for (let j = 0; j < BOARDHEIGHT; j++) {
-        for (let i = 0; i < BOARDWIDTH; i++) {
-            if (i>=2) {
-                let candyAvailable = [];
-                if (array[j][i-2].name === array[j][i-1].name) {
-                    const candyToRemove = array[j][i-2];
-                    console.log(candyToRemove);
-                    for (const candy of CANDIES) {
-                        if (candy !== candyToRemove.name) {
-                            candyAvailable.push(candy);
-                        }
-                    }
-
-                    console.log(candyAvailable);
-                    const candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
-                    console.log(candyRandom)
-                    array[j][i] = new Candy(candyRandom, j, i);
+        for (let i = 2; i < BOARDWIDTH; i++) {
+            if (array[j][i-2] === array[j][i-1]) {
+                
+                console.log("3 in a row " + "row " + j + " col " + i)
+                
+                // candyAvailable = candyAvailable.filter(candy => candy !== array[j][i-2]);
+                // console.log(candyAvailable);
+                
+                let candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
+                while (candyRandom === array[j][i-1]) {
+                    candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
                 }
-            }                
+                console.log(candyRandom)
+                array[j][i] = candyRandom;
+                    
+            //         for (const candy of CANDIES) {
+            //             if (candy !== candyToRemove.name) {
+            //                 candyAvailable.push(candy);
+            //             }
+            //         }
+            //         console.log(candyAvailable);
+            //         const candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
+            //         console.log(candyRandom)
+            //         array[j][i] = new Candy(candyRandom, j, i);
+            //     }
+            // }                
         }
     }   
     return array;     
-}
+}}
 
-const fillRowsThreeAndBelow = array => {
+const filterColumns = array => {
     for (let j = 2; j < BOARDHEIGHT; j++) {
         for (let i = 0; i < BOARDWIDTH; i++) {
-            let candyAvailable = CANDIES.map((candy) => candy);
+            let candyAvailable = CANDIES;
             if (array[j-2][i].name === array[j-1][i].name) {
-                candyAvailable = candyAvailable.filter((candy) => candy.name !== array[j-1][i].name);
-                console.log(candyAvailable);
-                if (i >= 2) {
-                    if (array[j][i-2].name === array[j][i-1].name) {
-                        candyAvailable = candyAvailable.filter((candy) => candy.name !== array[j][i-2].name);
-                        console.log(candyAvailable);
+                for (let k=0; k<candyAvailable.length; k++) {
+                    if (candyAvailable[k] === array[j-2][i].name) {
+                        candyAvailable.splice(k, 1);
                     }
                 }
-            }
-            
-            const candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
-            array[j][i] = new Candy(candyRandom, j, i);
+                console.log(candyAvailable)
+                // if (i >= 2) {
+                //     if (array[j][i-2].name === array[j][i-1].name) {
+                //         if (array[j][i-2].name !== candyToRemove1) {
+                //             candyToRemove2 = array[j][i-2];
+                //         }
+                //         for (const candy of CANDIES) {
+                //             if (candy !== candyToRemove1.name) {
+                //                 if (candy !== candyToRemove2.name) {
+                //                     candyAvailable.push(candy);
+                //                 }}}}}
+                // for (const candy of CANDIES) {
+                //     if (candy !== candyToRemove1.name) {
+                //         candyAvailable.push(candy);
+                //         } }
+                const candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
+                array[j][i] = new Candy(candyRandom, j, i);
+
+            //     if (i >= 2) {
+            //         if (array[j][i-2].name === array[j][i-1].name) {
+            //             const candyToRemove2 = array[j][i-2];
+            //             console.log(candyToRemove2);
+            //             for (const candy of CANDIES) {
+            //                 if (candy !== candyToRemove1.name) {
+            //                     if (candy !== candyToRemove2.name) {
+            //                         candyAvailable.push(candy);
+            //                     }
+            //                 }
+            //             } }
+            //         else {
+            //             for (const candy of CANDIES) {
+            //                 if (candy !== candyToRemove1.name) {
+            //                     candyAvailable.push(candy);
+            //                     }                   
+            //                 }}}
+            // console.log(candyAvailable)
+            // const candyRandom = candyAvailable[Math.floor(Math.random() * candyAvailable.length)];
+            // array[j][i] = new Candy(candyRandom, j, i);
             
         }
-    }  
+    } 
     return array;      
-}
-
+}}
+// --------------------------------------------------
 const paintTarget = event => {
     const candyTarget = event.target;
     // console.log(candyTarget.className);
@@ -113,19 +154,65 @@ const checkTargets = array => {
     return candyTargetArray;
 }
 
+const fillInCandies = array => {
+    array.forEach(rowArray => {
+        rowArray.forEach(candy => {
+            $(`.${candy.row}.${candy.col}`).addClass(`${candy.name}`);
+        })
+    })
+}
 
+const checkCandiesInARow = array => {
+    let sum = 0;
+    for (let j=BOARDHEIGHT-1; j>=0; j--) {
+        for (let i=2; i<BOARDWIDTH; i++) {
+            if (sum < 1) {
+                if (array[j][i-2].name === array[j][i-1].name && array[j][i-1].name === array[j][i].name) {
+                    array[j][i-2].remove = true;
+                    array[j][i-1].remove = true;
+                    array[j][i].remove = true;
+                    sum += 1;
+                    }      
+                }                 
+            }
+        }
+    return array;
+}
 
-const render = array => {
+const markCandiesToRemove = array => {
+    array.forEach(rowArray => {
+        rowArray.forEach(candy => {
+            if (candy.remove === true) {
+                $(`.${candy.row}.${candy.col}`).addClass("scored");
+            }            
+        })
+    })
+}
+
+const removeCandiesFromArray = array => {
+    for (let j=0; j<BOARDHEIGHT; j++) {
+        for (let i=0; i<BOARDWIDTH; i++) {
+            if (array[j][i].remove === true) {
+                array[j][i]
+            }
+        }
+    }
+    return array;
+}
+
+const render = () => {
     $container.empty();
     const $gameBoard = $("<div>").addClass("game-board");
-    candyTargetArray = checkTargets(newGameArray);
-    for (let j = 0; j < newGameArray.length; j++) {
-        const $row = $("<div>").addClass("row");
-        for (let i=0; i < array[j].length; i++) {
-            const $candyImg = $("<div>").addClass(`${array[j][i].name}`);
-            $candyImg.addClass(`${array[j][i].row}`);
-            $candyImg.addClass(`${array[j][i].col}`);
-            $candyImg.click(getCandyTarget);//use on click and run a function
+    // candyTargetArray = checkTargets(newGameArray);
+    for (let j = 0; j < BOARDHEIGHT; j++) {
+        const $row = $("<div>").addClass(`row`);
+        for (let i=0; i < BOARDWIDTH; i++) {
+            const $candyImg = $("<div>").addClass(`row${j}`);
+            $candyImg.addClass(`col${i}`);
+            // $candyImg.addClass(`${array[j][i].row}`);
+            // $candyImg.addClass(`${array[j][i].col}`);
+            // $candyImg.click(getCandyTarget);//use on click and run a function
+            // $candyImg.draggable();
             $row.append($candyImg);
         }
         $gameBoard.append($row);
@@ -140,9 +227,17 @@ const main = () => {
     
 
     filterRows(newGameArray);
-    // fillRowsThreeAndBelow(newGameArray);
+    // filterColumns(newGameArray);
     console.log(newGameArray);
-    // $container.append(render(newGameArray));
+    render();
+    fillInCandies(newGameArray);
+    checkCandiesInARow(newGameArray);
+    markCandiesToRemove(newGameArray);
+    console.log(newGameArray);
+    removeCandiesFromArray(newGameArray);
+    console.log(newGameArray);
+    
+    
     
     
     
