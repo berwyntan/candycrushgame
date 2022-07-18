4d85aaccf1daa546f6d33b09e81f07caf423ef47
Berwyn Tan <berwyntan@gmail.com>
2022-07-18 19:43:19 +0800

    working on set timeout fn

--- app.js
+++ app.js
@@ -18,8 +18,92 @@ const createRandomArray = () => {
     return itemBoard;        
 }
 
+// check row from bottom
+const checkRow = () => {
+    let checkCount = 0;
+    for (let j = BOARDHEIGHT-1; j >=0; j--) {
+        for (let i=2; i < BOARDWIDTH; i++) {
+            if (checkCount < 1) {
+                let candyFirst = itemBoard[j][i-2];
+                let candySecond = itemBoard[j][i-1];
+                let candyThird = itemBoard[j][i];
+                if (candyFirst === candySecond && candySecond === candyThird && candyThird !=="empty") {
+                    itemBoard[j][i-2] = "empty";
+                    itemBoard[j][i-1] = "empty";
+                    itemBoard[j][i] = "empty";
+                    checkCount +=1;
+                    
+                }
+            }}
+    setTimeout(() => {render(), 2000});
+            
+}}
 
+const gravity = () => {
+    for (let j = BOARDHEIGHT-1; j >=1; j--) {
+        for (let i=0; i < BOARDWIDTH; i++) {
+            let candy = itemBoard[j][i];
+            let candyAbove = itemBoard[j-1][i];
+            let temp;
+            if (candy === "empty") {
+                temp = itemBoard[j-1][i];
+                itemBoard[j][i] = candyAbove;
+                itemBoard[j-1][i] = "empty";
+            }}
 
+        }
+    setTimeout(render(), 2000);
+    }
+
+let dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", event => {
+    console.log("dragging");
+});
+
+document.addEventListener("dragstart", event => {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", event => {
+    // reset the transparency
+    event.target.classList.remove("dragging");
+});
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", event => {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", event => {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.classList.contains("dropzone")) {
+    event.target.classList.add("dragover");
+    }
+});
+
+document.addEventListener("dragleave", event => {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+    }
+});
+
+document.addEventListener("drop", event => {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged element to the selected drop target
+    if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+    }
+});
 
 const render = () => {
     $container.empty();
@@ -31,6 +115,7 @@ const render = () => {
             const $candy = $("<img>");
             $candy.attr("id", `row${j}col${i}`);
             $candy.attr("src", `./images/${itemBoard[j][i]}.png`);
+            $candy.attr("draggable", "true");
             
             $row.append($candy);
         }
@@ -45,9 +130,11 @@ const main = () => {
     itemBoard = createRandomArray();
     console.log(itemBoard);
     render();
-    
-    
-   }
+    checkRow();
+    gravity();
+
+
+}
 
 
 $(main);
